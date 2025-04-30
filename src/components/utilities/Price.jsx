303049e548b59ac849afaa660e83d717f14f06da
Kuldeep chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2025-04-30 12:51:24 +0530

    for mac heading

--- src/components/utilities/Price.jsx
+++ src/components/utilities/Price.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Price = ({ text, span, CustomClass, firstText,OtherText }) => {
   return (
-    <p className={`font-cinzel font-semibold tracking-wide uppercase bg-gradient-to-r from-[#705118] to-[#A39264] bg-clip-text text-transparent text-center flex flex-wrap justify-center items-baseline ${CustomClass}`}>
+    <p className={`font-cinzel  font-semibold tracking-wide uppercase bg-gradient-to-r from-[#705118] to-[#A39264] bg-clip-text text-transparent text-center flex flex-wrap justify-center items-baseline ${CustomClass ? CustomClass : "gradient-text"}`}>
       {text?.split(' ').map((word, index) => (
         <span key={index} className={`flex items-center mr-2 text-[20px] md:text-[32px] ${OtherText}`}>
           <span className={` text-[22px] md:text-[36px] ${firstText}`}>{word.charAt(0)}</span>
