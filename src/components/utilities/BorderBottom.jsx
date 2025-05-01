86f26572dff5af832b99eb3d21712fa4851c303c
Kuldeep chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2025-05-01 14:35:16 +0530

    for border

--- src/components/utilities/BorderBottom.jsx
+++ src/components/utilities/BorderBottom.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BorderBottom = ({ CustomClass, isWhite = false }) => {
   return (
-    <div className={`h-[1px] ${isWhite ? 'bg-gradient-to-l from-transparent via-[#ffff] to-transparent' : 'bg-gradient-to-l from-transparent via-[#705118] to-transparent'} w-[150px] ${CustomClass}`}>
+    <div className={`h-[1px] ${isWhite ? 'gradient-white-border bg-gradient-to-l from-transparent via-[#ffff] to-transparent' : 'gradient-color-border bg-gradient-to-l from-transparent via-[#705118] to-transparent'} w-[150px] ${CustomClass}`}>
     </div>
   )
 }
