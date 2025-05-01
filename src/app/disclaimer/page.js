1bf7a8a4770cc23ef450fd20608f9ea3c0fb913c
Kuldeep chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2025-05-01 14:46:01 +0530

    disclaimer

--- src/app/disclaimer/page.js
+++ src/app/disclaimer/page.js
@@ -16,32 +16,32 @@ const Disclaimer = () => {
           </h2>
   
           <p style={{ paddingTop: '10px', textAlign: 'justify', lineHeight: '24px' }}>
-            <b style={{ display: 'block', fontSize: '15px' }}>Disclaimer</b>
-            This website is only for the purpose of providing information regarding real estate projects in different regions. By accessing this website...
+            <b style={{  fontSize: '15px' }}>Disclaimer: </b>
+            This website is only for the purpose of providing information regarding real estate projects in different regions, by accessing this website.
             <br />
-            Please also note that the company has not verified the information and the compliances...
+            Please also note that the company has not verified the information and the compliances.
             <br />
-            <strong>This site is for information purpose only and should not be treated as the official website.</strong>
+            <strong>This site is for information purposes only and should not be treated as the official website.</strong>
           </p>
   
           <p style={{ paddingTop: '10px', textAlign: 'justify', lineHeight: '24px' }}>
-            <b style={{ display: 'block', fontSize: '15px' }}>Privacy Policy</b>
-            In our endeavor and commitment of protecting your personal information, we have designed this comprehensive privacy policy...
+            <b style={{ fontSize: '15px' }}>Privacy Policy: </b>
+            In our endeavour and commitment to protecting your personal information, we have designed this comprehensive privacy policy.
           </p>
   
           <p style={{ paddingTop: '10px', textAlign: 'justify', lineHeight: '24px' }}>
-            <b style={{ display: 'block' }}>Updation of privacy policy</b>
-            This privacy policy is subject to undergo change and review without any prior notice...
+            <b >Updating privacy policy: </b>
+            This privacy policy is subject to change and review without any prior notice.
           </p>
   
           <p style={{ paddingTop: '10px', textAlign: 'justify', lineHeight: '24px' }}>
-            <b style={{ display: 'block' }}>User information</b>
-            By using our website, you agree to abide by the rules laid out by us and consent to collection and use of all such information...
+            <b >User information: </b>
+            By using our website, You agree to abide by the rules we have laid out and consent to the collection and use of all such information.
           </p>
   
           <p style={{ paddingTop: '10px', textAlign: 'justify', lineHeight: '24px' }}>
-            <b style={{ display: 'block' }}>Security</b>
-            To ensure security while transferring sensitive information, all the ongoing transmissions between client and server are encrypted...
+            <b >Security: </b>
+            To ensure security while transferring sensitive information, all the ongoing transmissions between client and server are encrypted.
           </p>
         </div>
   
