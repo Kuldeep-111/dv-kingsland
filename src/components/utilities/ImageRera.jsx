8227de03945ba8b3e40b4a61e94327cfd89d2118
Kuldeep chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2025-05-02 12:09:42 +0530

    rera

---
+++ src/components/utilities/ImageRera.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+const ImageRera = () => {
+  return (
+    <ul className="absolute left-[0] bottom-[0] py-[6px] px-[6px] flex  place-items-center justify-between bg-[#191616] w-full"><li className="leading-[normal] text-[#fff] normal-case tracking-[normal] text-[8px] md:text-[10px]">Artistic Impression</li><li className="leading-[normal] text-[#fff] normal-case tracking-[normal] text-[8px] md:text-[10px]">MP/19/2023/01042</li><Link className="text-[8px] normal-case text-[#fff] leading-[0] md:text-[10px] tracking-[normal] leading-[normal]" href="https://rera.odisha.gov.in/" target="_blank">https://rera.odisha.gov.in/</Link></ul>
+  )
+}
+
+export default ImageRera
