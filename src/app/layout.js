77bc451c74d1cd36d98080628e09017a8f8c457d
Kuldeep chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2025-05-09 10:54:30 +0530

    meta pixels script

--- src/app/layout.js
+++ src/app/layout.js
@@ -53,6 +53,25 @@ export default function RootLayout({ children }) {
   gtag('js', new Date());
   gtag('config', 'G-07JFDJWP87');`}
 </script>
+
+{/* meta pixel start */}
+<script>{`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1656003297922168');
+fbq('track', 'PageView');`}
+</script>
+<noscript><img height="1" width="1" style={{ display: "none" }}
+src="https://www.facebook.com/tr?id=1656003297922168&ev=PageView&noscript=1"
+/></noscript>
+
+{/* meta pixel end */}
         </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
